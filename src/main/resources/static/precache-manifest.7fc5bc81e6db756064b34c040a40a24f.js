2a375cda60e24f6f05936d4c6ecc03025d26e571
ladovat <ladislav.barabas@gmail.com>
2020-09-06 11:24:24 +0200

    Includes frontend static build

---
+++ src/main/resources/static/precache-manifest.7fc5bc81e6db756064b34c040a40a24f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb220fed2374da33bb3a32aea4b9406",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aab37fe61812870e3a00",
+    "url": "/static/css/2.9483eb91.chunk.css"
+  },
+  {
+    "revision": "325441fb71a17f13a37c",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "aab37fe61812870e3a00",
+    "url": "/static/js/2.39495da2.chunk.js"
+  },
+  {
+    "revision": "7c6de3322eb4ea2ab32454492afd4f37",
+    "url": "/static/js/2.39495da2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "325441fb71a17f13a37c",
+    "url": "/static/js/main.d1fcc4e5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
